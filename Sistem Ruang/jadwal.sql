d902d1ea9547417e6330506a44cb895c0526447e
Royyan <cakroyyan@gmail.com>
2016-10-26 15:20:12 +0700

    Add files via upload

---
+++ Sistem Ruang/jadwal.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Community v12.2.6 (64 bit)
+MySQL - 5.6.25 : Database - jadwal
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jadwal` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `jadwal`;
+
+/*Table structure for table `jadwal` */
+
+DROP TABLE IF EXISTS `jadwal`;
+
+CREATE TABLE `jadwal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(1000) DEFAULT NULL,
+  `tgl` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `jadwal` */
+
+insert  into `jadwal`(`id`,`nama`,`tgl`) values 
+(1,'Kegiatan Perpus','2016-10-26'),
+(2,'Kehiatan LP2M','2016-10-10');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
